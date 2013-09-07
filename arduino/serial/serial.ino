d97074f9c9468d5af42992dac2678b4b8506c526
Nelson Zhang <nelsonz@acm.org>
2013-09-07 01:10:04 -0700

    controller

--- arduino/serial/serial.ino
+++ arduino/serial/serial.ino
@@ -1,22 +1,63 @@
+#include <Servo.h> 
+
 String incomingString;
+Servo pan, tilt;
+
+int theta = 0, phi = 0;
+int panPin = 10, tiltPin = 11, buttonPin = 12; 
 
 void setup(){
   Serial.begin(9600);
   Serial.flush();
+  pan.attach(panPin);
+  tilt.attach(tiltPin);
+  pinMode(buttonPin, OUTPUT);
 }
 
 void loop() {
-  delay(1000);
+  // delay(10);
+  
   // Check if there's incoming serial data.
   while (Serial.available() > 0) {
     // Read a byte from the serial buffer.
     char incomingByte = (char)Serial.read();
+    // stop reading for now if we reach a break character
+    if (incomingByte == '/') {
+      break; 
+    }
     incomingString += incomingByte;
   }
 
-  if (incomingString != "") {
-    // Message completely received
-    Serial.println(incomingString);
-    incomingString = "";
+  Serial.println(incomingString);
+    
+  if (incomingString == "on") {
+      press();
   }
+  else if (incomingString == "off") {
+      release();
+  }
+  else if (incomingString == "aim") {
+      aim();
+  }
+  else if (incomingString == "line") {
+      line();
+  }
+    
+  incomingString = "";
+}
+
+void press() {
+  digitalWrite(buttonPin, HIGH);
+}
+
+void release() {
+  digitalWrite(buttonPin, LOW);
+}
+
+void aim() {
+  
+}
+
+void line() {
+  
 }
