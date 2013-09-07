75db459ace74d607b19308fbb802c0b849fa618a
Jian Leong <jianwei1801@gmail.com>
2013-09-07 01:31:22 -0700

    added highlevel systems

--- arduino.py
+++ arduino.py
@@ -1,18 +1,23 @@
 import serial
 
-class lazr:
+
+class LazrSystem(object):
+
 	def __init__(self, port='/dev/ttyACM0', bps=9600):
 		self.ser = serial.Serial(port, bps)
+    self.mode = False
 
 	def charge(self, mode):
-		self.ser.write("on/" if mode else "off/")
-	
+    if mode != self.mode:
+        self.ser.write("on/" if mode else "off/")
+        self.mode = mode
+
 	def aim(self, x, y):
 		self.ser.write("aim/")
 		pan = x
 		tilt = y
 		self.ser.write(chr(pan) + "/" + chr(tilt) + "/")
-		
+
 	def line(self, x1, y1, x2, y2, speed = 5):
 		self.ser.write("line/")
 		pan1 = x1
@@ -20,4 +25,3 @@ class lazr:
 		pan2 = x2
 		tilt2 = y2
 		self.ser.write(chr(pan1) + "/" + chr(tilt1) + "/" + chr(pan2) + "/" + chr(tilt2) + "/" + chr(speed) + "/")
-
