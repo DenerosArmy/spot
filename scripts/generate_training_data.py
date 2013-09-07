ecc34393ce9ded202bcb2e13d0ca958bf7b6cafa
Nikita Kitaev <nikitakit@gmail.com>
2013-09-07 02:39:42 -0700

    Better filenames for training data files

--- scripts/generate_training_data.py
+++ scripts/generate_training_data.py
@@ -6,6 +6,7 @@ Usage: python scripts/generate_training_data.py
 from SimpleCV import Camera
 import os
 import settings
+import time
 c = Camera(settings.camera_index)
 name = raw_input("Object Name: ")
 base_path = settings.base_path
@@ -14,7 +15,7 @@ i = 0
 print "Press [enter] to take another image, or use [q] to exit."
 while raw_input() != "q":
     img = c.getImage()
-    img.save(os.path.join(base_path, "training_data", name, "{}.jpg".format(i)))
+    img.save(os.path.join(base_path, "training_data", name, "{}_{}_{}.jpg".format(name, int(time.time()), i)))
     print "Saved image ", i
     i += 1
     print "Press [enter] to take another image, or use [q] to exit."
