b9ce360f83803d38cf83487c26375136c5e25ed8
Nikita Kitaev <nikitakit@gmail.com>
2013-09-07 02:17:36 -0700

    Fix whitespace errors in arduino.py

--- arduino.py
+++ arduino.py
@@ -3,25 +3,25 @@ import serial
 
 class LazrSystem(object):
 
-	def __init__(self, port='/dev/ttyACM0', bps=9600):
-		self.ser = serial.Serial(port, bps)
-    self.mode = False
+    def __init__(self, port='/dev/ttyACM0', bps=9600):
+        self.ser = serial.Serial(port, bps)
+        self.mode = False
 
-	def charge(self, mode):
-    if mode != self.mode:
-        self.ser.write("on/" if mode else "off/")
-        self.mode = mode
+    def charge(self, mode):
+        if mode != self.mode:
+            self.ser.write("on/" if mode else "off/")
+            self.mode = mode
 
-	def aim(self, x, y):
-		self.ser.write("aim/")
-		pan = x
-		tilt = y
-		self.ser.write(chr(pan) + "/" + chr(tilt) + "/")
+    def aim(self, x, y):
+        self.ser.write("aim/")
+        pan = x
+        tilt = y
+        self.ser.write(chr(pan) + "/" + chr(tilt) + "/")
 
-	def line(self, x1, y1, x2, y2, speed = 5):
-		self.ser.write("line/")
-		pan1 = x1
-		tilt1 = y1
-		pan2 = x2
-		tilt2 = y2
-		self.ser.write(chr(pan1) + "/" + chr(tilt1) + "/" + chr(pan2) + "/" + chr(tilt2) + "/" + chr(speed) + "/")
+    def line(self, x1, y1, x2, y2, speed = 5):
+        self.ser.write("line/")
+        pan1 = x1
+        tilt1 = y1
+        pan2 = x2
+        tilt2 = y2
+        self.ser.write(chr(pan1) + "/" + chr(tilt1) + "/" + chr(pan2) + "/" + chr(tilt2) + "/" + chr(speed) + "/")
