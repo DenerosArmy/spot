8dbfa5d7e9e110b06156bdf1a5234e2f3e21c653
Nikita Kitaev <nikitakit@gmail.com>
2013-09-06 22:32:50 -0700

    Update data collection script for new format

--- scripts/generate_training_data.py
+++ scripts/generate_training_data.py
@@ -1,12 +1,18 @@
 """Saves test samples"""
 from SimpleCV import Camera
-c = Camera(0)
+import os
+c = Camera(int(raw_input("Camera index:")))
 name = raw_input("Object Name: ")
+if os.environ["USER"] == "nikita":
+    base_path = "/home/nikita/dev/pinkie/"
+else:
+    base_path = "/Users/jian/Projects/Pinkie/"
+os.mkdir(os.path.join(base_path, "training_data", name))
 i = 0
 print "Press [enter] to take another image, or use [q] to exit."
 while raw_input() != "q":
     img = c.getImage()
-    img.save("/Users/jian/Projects/Pinkie/training_data/{0}_{1}.jpg".format(name, i))
+    img.save(os.path.join(base_path, "training_data", name, "{}.jpg".format(i)))
     print "Saved image ", i
     i += 1
     print "Press [enter] to take another image, or use [q] to exit."
